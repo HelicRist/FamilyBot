4c342577c685c961cbbb92809e6a840f3cf61a15
Heldingar <84403208+Heldingar@users.noreply.github.com>
2021-08-11 02:51:46 +0200

    tictactoe

---
+++ commands/fun/tictactoe.js
@@ -0,0 +1,17 @@
+
+const TicTacToe = require('discord-tictactoe');
+
+const game = new TicTacToe({ language: 'it' })
+
+module.exports = {
+    name: 'tictactoe',
+    aliases: ['ttt','tic'],
+    description: 'Gioca a TicTacToe',
+    category :'fun',
+    run: async (client, message, args) => {
+        console.log("start");
+
+        game.handleMessage(message);
+
+    }
+}
