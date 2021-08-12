6ab46bd2032c90725f91c099a83480f5ecb26106
Heldingar <84403208+Heldingar@users.noreply.github.com>
2021-08-12 22:14:06 +0200

    aggiornato win in bet

--- commands/bet/actions/win.js
+++ commands/bet/actions/win.js
@@ -1,79 +1,65 @@
-//TODO togli bet
+
 const { MessageEmbed } = require('discord.js');
 const fs = require("fs")
 
 module.exports = {
-    name: 'win',
-    aliases: ['vincita'],
-    description: 'Per riscuotere la vincita',
+    name: 'test',
+    aliases: ['test'],
+    description: 't',
     run: async (client, message, args) => {
-        let bets, winner, loser, punti, scommessa;
         let embedBets = new MessageEmbed()
             .setTitle("nullo")
             .setColor("#008f00")
-
-        if (isNaN(args[1]) || args[1] < 1) return message.reply('Non c\'è quel numero  pepega');
-
+        if (isNaN(args[1]) || parseInt(args[1] < 1)) return message.reply(':x: Bet non valida :x:');
         let indice = args[1] - 1
-        console.log(indice);
-        fs.readFile('data/bet.json', 'utf-8', (err, data) => {
-            if (err) {
-                throw err;
-            }
-            const bet = JSON.parse(data.toString());
+        let bets = []
+        let winner, loser, punti, scommessa,classifica;
+        message.react("👍")
+        
+        fs.readFile('data/bet2.json', 'utf-8', (err, dataB) => {
+            if (err) { throw err; }
+            bet = JSON.parse(dataB.toString());
             bets = bet.bets
-            if (indice > bets.length) return message.reply('non c\'è quel numero  (troppo grande)');
-            //
-            if (bets[indice][2] != message.author.id && bets[indice][3] != message.author.id ) return message.reply('non hai partecipato a quella bet');
-            winner = message.author.id;
-            if (bets[indice][2] === winner) { loser = bets[indice][3]; } else if (bets[indice][3] === winner) { loser = bets[indice[2]] }
-            punti = parseInt(bets[indice][1])
+            
+            if (!(message.author.id === bets[indice][2] || message.author.id === bets[indice][3])) { return message.reply('Non hai partecipato alla bet') }
+            winner = message.author.id
+            if (message.author.id === bets[indice][2]) {
+                loser = bets[indice][3]
+            } else if (message.author.id === bets[indice][3]) {
+                loser = bets[indice][2]
+            }
             scommessa = bets[indice][0]
-            message.react("👍")
+            punti = parseInt(bets[indice][1])
+            client.on('messageReactionAdd', (reaction, user) => {
+                if (reaction.emoji.name === "👍" && user.id === loser) {
+                    fs.readFile('data/classifica2.json', 'utf-8', (err, dataC) => {
+                        if (err) { throw err; }
+                        classifica = JSON.parse(dataC.toString());
 
-            fs.readFile('data/classifica.json', 'utf-8', (err, data) => {
-                if (err) {
-                    throw err;
-                }
-                const classifica = JSON.parse(data.toString());
-                //inveritre in lose
-                client.on('messageReactionAdd', (reaction, user) => {
-                    if (reaction.emoji.name === "👍" && user.id === loser) {
-                        let puntiWin = classifica[winner] += punti
-                        let puntiLos = classifica[loser] -= punti
+                        classifica[winner] += punti
+                        classifica[loser] -= punti
 
-                        const data2 = JSON.stringify(classifica);
-                        fs.writeFile('data/classifica.json', data2, (err) => {
-                            if (err) {
-                                throw err;
-                            }
-                        });//write classificajson
-                    }
+                        const dataC2 = JSON.stringify(classifica);
+                        fs.writeFile('data/classifica2.json', dataC2, (err) => {
+                            if (err) { throw err; }
+
+                        });//write c
+                        bet.bets.splice(indice, 1)
+                        const dataB2 = JSON.stringify(bet);
+                        fs.writeFile('data/bet2.json', dataB2, (err) => {
+                            if (err) { throw err; }
+                        });//write b
+
+                    });//leggi classifica
                     embedBets
-                        .setTitle("Bet Riscossa")
-                        .setThumbnail("https://cdn3.iconfinder.com/data/icons/casino-and-gambling-1/50/Casino_And_Gambling_Casino_chips-44-512.png")
-                        .setDescription(`<@${winner}> hai vinto ${punti} :coin: contro <@${loser}>\n${scommessa}`)
-                        .setFooter("Che la fortuna vi arrida")
-                    message.channel.send(embedBets)
-                });//react
-                
-                
-            });//leggi classificajson
-            
-            
-            bet.bets.splice(indice, 1)
-            const dataBet = JSON.stringify(bet);
-            // write JSON string to a file
-            fs.writeFile('data/bet.json', dataBet, (err) => {
-                if (err) {
-                    throw err;
-                }
-
-
+                    .setTitle("Bet Riscossa")
+                    .setThumbnail("https://cdn3.iconfinder.com/data/icons/casino-and-gambling-1/50/Casino_And_Gambling_Casino_chips-44-512.png")
+                    .setDescription(`<@${winner}> hai vinto ${punti} :coin: contro <@${loser}>\n${scommessa}`)
+                    .setFooter("Che la fortuna vi arrida")
+                message.channel.send(embedBets)
+            message.reply(bets[indice])
+         }
             });
-
-
-        });//leggi betjson
-
+        });//leggi bet
     }
 }
