621cc6bcafeea2ba445cfb06dc2acb3afc132528
Heldingar <84403208+Heldingar@users.noreply.github.com>
2021-08-12 23:49:04 +0200

    add clear

---
+++ commands/moderation/clear.js
@@ -0,0 +1,35 @@
+
+module.exports = {
+    name: "clear",
+    aliases: ["pulisci"],
+    description: `cancella messaggi`,
+    category: "moderation",
+    
+    run: async (client, message, args) => {
+    const amount = args.join(' '); 
+    let id = message.member.id;
+    if ( id == "342343548718284801" ) {
+        if (!amount) return message.reply('Specifica quanti messaggi eliminare'); 
+        if (isNaN(amount)) return message.reply('Devi mettere un numero'); 
+
+        if (amount > 100) return message.reply('Woo più di 100 son troppi'); 
+        if (amount < 1) return message.reply('Deve essere almeno 1'); 
+
+        
+        await message.delete();
+
+        message.channel.bulkDelete(amount).then(() => {
+            message.channel.send("**Stealth!**")
+            .then(message => {
+                message.delete({ timeout: 1000 });
+            });
+        });
+    } else {
+        const embed = new MessageEmbed()
+            .setTitle(':x: Error')
+            .setColor(0xff0000)
+            .setDescription('Non puoi ');
+            message.channel.send(embed);    }
+
+    }
+}
