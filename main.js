4d63964dd229af6e2f386b60a79e674f3bcd3291
Heldingar <84403208+Heldingar@users.noreply.github.com>
2021-08-14 17:13:14 +0200

    aggiunto DailyPic (alle 18 30)

--- main.js
+++ main.js
@@ -67,7 +67,39 @@ client.on('ready', () => {
         client.user.setActivity(status, { type: "STREAMING", url: 'https://www.twitch.tv/relaxbeats' });
 
     }, 5000)
+//
+         let scheduledMessage = new cron.CronJob('00 30 18 * * *', () => {
 
+         const hembed = new MessageEmbed();
+           if(r50()){
+            akaneko.neko().then((imageURL) => {
+                console.log(imageURL);
+                hembed
+                    .setTitle(" :lollipop:  DAILY PIC  :purple_heart: ")
+                    .setColor("#FFDEDE")
+                    .setImage(imageURL)
+                client.channels.cache.get("821068653348913223").send({embed:hembed})
+            });
+
+        }else{
+            akaneko.foxgirl().then((imageURL) => {
+                
+                console.log(imageURL);
+                hembed
+                    .setTitle(" :lollipop:  DAILY PIC  :purple_heart: ")
+                    .setColor("#FFDEDE")
+                    .setImage(imageURL)
+                client.channels.cache.get("844291548140011540").send({embed:hembed})
+                });
+        }
+
+    });
+              
+          scheduledMessage.start()          
+
+          function r50(){
+              return Math.random() < 0.5;
+            }
 });
 
 client.login(process.env.TOKEN);
