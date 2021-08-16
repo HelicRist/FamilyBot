89fbf74e0cdbd4160fca6af599351b1873f12be1
Heldingar <84403208+Heldingar@users.noreply.github.com>
2021-08-16 20:28:49 +0200

    acambiato daily pic in main per test

--- main.js
+++ main.js
@@ -68,7 +68,7 @@ client.on('ready', () => {
 
     }, 5000)
 //
-         let scheduledMessage = new cron.CronJob('00 28 20 * * *', () => {
+         let scheduledMessage = new cron.CronJob('00 43 20 * * *', () => {
         console.log("DAILY PIC")
          const hembed = new MessageEmbed();
            if(r50()){
