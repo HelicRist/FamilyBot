37509805062dc4bea014ec647393acba514ccf2b
Heldingar <84403208+Heldingar@users.noreply.github.com>
2021-08-16 03:09:46 +0200

    aggiungo comando in fun

---
+++ commands/fun/mood.js
@@ -0,0 +1,39 @@
+const { MessageEmbed } = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+    name: 'mood',
+    aliases: ['billie'],
+    description: 'Get a Billies\'s cute vid <3',
+    category: 'fun',
+
+    run: async (client, message, args) => {
+        let urls = [
+            "Rnn9ESx",
+            "e1QfsGK",
+            "hjcvQOF",
+            "2D7lIQf",
+            "T5CqJpL",
+            "EG7hzG3",
+            "JdtIqCw",
+            "u6W3jUJ",
+            "9cNCgMZ",
+            "sFyudgT",
+            "NvSlcLN",
+            "jcQ8NLF",
+            "00hdkLm",        
+        ]
+        let files = fs.readdirSync('media/billie');
+        message.channel.send(" Cercando...").then(msg => {
+           console.log( urls[Math.floor(Math.random() * urls.length)]);
+            message.channel.send(" 💜 Billie Mood :heart_exclamation: ", {
+                files: [
+                    `https://i.imgur.com/${urls[Math.floor(Math.random() * urls.length)]}.mp4`,
+                ]
+            }).then(mex => {
+                msg.delete()
+            })
+        })
+
+    }
+}
