17d4dc84c9d24904faf42b046f8817d6ec4066df
Heldingar <84403208+Heldingar@users.noreply.github.com>
2021-08-18 18:46:35 +0200

    Update ciao.js

--- commands/ciao/ciao.js
+++ commands/ciao/ciao.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "ciao",
     description: `leonardo ha problemi`,
     aliases: ["leonardo ha problemi"],
-    usage: leonardo ha problemi`,
+    usage:` leonardo ha problemi`,
     category: "ciao",
     run: async (client, message, args) => {
 
