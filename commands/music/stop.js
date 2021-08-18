6b35c7e0f5230a52caacbe9e959425750779568c
Heldingar <84403208+Heldingar@users.noreply.github.com>
2021-08-18 18:47:17 +0200

    Update stop.js

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "stop",
     description: `leonardo ha problemi`,
     aliases: ["leonardo ha problemi"],
-    usage: leonardo ha problemi`,
+    usage: `leonardo ha problemi`,
     category: "music",
     run: async (client, message, args) => {
 
