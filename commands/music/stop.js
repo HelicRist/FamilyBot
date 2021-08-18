aa8055bc7a4ee5c93adcc398fd48d71692ed57ef
Heldingar <84403208+Heldingar@users.noreply.github.com>
2021-08-18 15:21:26 +0200

    stopjs

---
+++ commands/music/stop.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    name: "stop",
+    description: `leonardo ha problemi`,
+    aliases: ["leonardo ha problemi"],
+    usage: leonardo ha problemi`,
+    category: "music",
+    run: async (client, message, args) => {
+
+message.reply("Coming soon");
+}
+}
