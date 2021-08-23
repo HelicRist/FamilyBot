dffc2fb71d419df9c7f805a65579fee07e172634
Heldingar <84403208+Heldingar@users.noreply.github.com>
2021-08-23 05:07:09 +0200

    aggiornamenti daiy pic
    - hentai e non sfw
    - tag @here sopra 
    vediamo se alle 14 45 (o 16 46) funziona

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 const config = require('../config.json');
 const cron = require('cron');
 const akaneko = require('akaneko');
-const {  MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: 'ready',
@@ -20,37 +20,25 @@ module.exports = {
 
         }, 5000)
         //
-        let scheduledMessage = new cron.CronJob('00 45 13 * * *', () => {
+        let scheduledMessage = new cron.CronJob('00 45 14 * * *', () => {
 
             const hembed = new MessageEmbed();
-            if (r50()) {
-                akaneko.neko().then((imageURL) => {
-                    console.log(imageURL);
+
+            akaneko.nsfw.hentai().then((imageURL) => {
+
+                console.log(imageURL);
+                message.channel.send("↓↓ @here ↓↓").then(() => {
                     hembed
                         .setTitle(" :lollipop:  DAILY PIC  :purple_heart: ")
                         .setColor("#FFDEDE")
                         .setImage(imageURL)
-                    client.channels.cache.get("821068653348913223").send({ embed: hembed })
-                });
-
-            } else {
-                akaneko.foxgirl().then((imageURL) => {
-
-                    console.log(imageURL);
-                    hembed
-                        .setTitle(" :lollipop:  DAILY PIC  :purple_heart: ")
-                        .setColor("#FFDEDE")
-                        .setImage(imageURL)
-                    client.channels.cache.get("844291548140011540").send({ embed: hembed })
-                });
-            }
+                    client.channels.cache.get("821068653348913223").send(hembed)
+                })
+            });
 
         });
 
         scheduledMessage.start()
 
-        function r50() {
-            return Math.random() < 0.5;
-        }
     }
 }
